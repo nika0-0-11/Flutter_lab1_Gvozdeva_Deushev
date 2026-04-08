b8fdb079eaf156a420c0267c017551e691fc84e5
nika0-0-11 <nika_gvozdeva0.0@mail.ru>
2026-04-08 15:19:18 +0300

    Add main entry point with CLI loop - Гвоздева

--- bin/todo_app.dart
+++ bin/todo_app.dart
@@ -1,23 +1,44 @@
+import 'dart:io';
+import 'package:todo_app/todo.dart';
+import 'package:todo_app/todo_repository.dart';
+
+// void main() {
+//   print("Hello wolrd");
+
+//   String name = "Nika";
+//   String? nam2 = null;
+
+//   var count = 0;
+//   var title = "Hello";
+//   var x = 5;
+//   // x = "dsf";
+
+//   final id;
+//   id = 5;
+//   const app = "Todo";
+
+//   print("$app $id ${count + 5}");
+
+//   List<String> test = ["язык", "уроки"];
+
+//   Map<String, dynamic> data = {"key" : 13};
+
+//   Set<int> id2 = {1, 2, 3};
+// }
+
 void main() {
-  print("Hello wolrd");
-
-  String name = "Nika";
-  String? nam2 = null;
-
-  var count = 0;
-  var title = "Hello";
-  var x = 5;
-  // x = "dsf";
-
-  final id;
-  id = 5;
-  const app = "Todo";
-
-  print("$app $id ${count + 5}");
-
-  List<String> test = ["язык", "уроки"];
-
-  Map<String, dynamic> data = {"key" : 13};
-
-  Set<int> id2 = {1, 2, 3};
+  TodoRepository repo = TodoRepository();
+  printMenu();
+  while (true) {
+    stdout.write("> ");
+    String? input = stdin.readLineSync();
+    if (input == null) {
+      continue;
+    }
+    input = input.trim();
+    bool shouldExit = handleCommand(repo, input);
+    if (shouldExit) {
+      break;
+    }
+  }
 }
--- lib/todo_repository.dart
+++ lib/todo_repository.dart
@@ -8,30 +8,29 @@ class TodoRepository {
   }
 
   void add(String title) {
-    if (title.trim().isEmpty)
-    {
+    if (title.trim().isEmpty) {
       throw ArgumentError("Задача не может быть пустой");
     }
 
     Todo todo = Todo(title.trim());
     _todos.add(todo);
+  }
 
-    void complete(int id) {
-      for (var todo in _todos) {
-        if (todo.id == id) {
-          todo.isDone = true;
-          return;
-        } 
+  void complete(int id) {
+    for (var todo in _todos) {
+      if (todo.id == id) {
+        todo.isDone = true;
+        return;
       }
-      throw ArgumentError("Задача с таким id не найдена");
     }
+    throw ArgumentError("Задача с таким id не найдена");
+  }
 
-    void delete(int id) {
-      for (int i = 0; i < _todos.length; i++) {
-        if (_todos[i].id == id) {
-          _todos.removeAt(i);
-          return;
-        } 
+  void delete(int id) {
+    for (int i = 0; i < _todos.length; i++) {
+      if (_todos[i].id == id) {
+        _todos.removeAt(i);
+        return;
       }
     }
   }
