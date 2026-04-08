268c8721cceb6928348b6614e2fa605a2d5aff02
nika0-0-11 <nika_gvozdeva0.0@mail.ru>
2026-04-08 14:21:02 +0300

    Added structure class

--- bin/todo_app.dart
+++ bin/todo_app.dart
@@ -1,3 +1,23 @@
 void main() {
   print("Hello wolrd");
+
+  String name = "Nika";
+  String? nam2 = null;
+
+  var count = 0;
+  var title = "Hello";
+  var x = 5;
+  // x = "dsf";
+
+  final id;
+  id = 5;
+  const app = "Todo";
+
+  print("$app $id ${count + 5}");
+
+  List<String> test = ["язык", "уроки"];
+
+  Map<String, dynamic> data = {"key" : 13};
+
+  Set<int> id2 = {1, 2, 3};
 }
